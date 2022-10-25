ffa8524d52161a448b80edf4658b0f0e84ddb3fc
xuetingfeng <coderxue@qq.com>
2022-10-25 16:53:32 +0800

    medium

---
+++ vuejs-challenges/medium/rawApi.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { reactive, isReactive, toRaw, markRaw } from "vue"
+
+const state = { count: 1 }
+const reactiveState = reactive(state)
+
+/**
+ * Modify the code so that we can make the output be true.
+*/
+console.log(toRaw(reactiveState) === state)
+
+/**
+ * Modify the code so that we can make the output be false.
+*/
+const info = markRaw({ count: 1 })
+const reactiveInfo = reactive(info)
+
+console.log(isReactive(reactiveInfo))
+
+</script>
+
+<template>
+  <div>
+    <p>
+      {{ reactiveState.count }}
+    </p>
+  </div>
+</template>
---
+++ vuejs-challenges/medium/切换器.vue
@@ -0,0 +1,27 @@
+<script setup lang='ts'>
+
+/**
+ * Implement a composable function that toggles the state
+ * Make the function work correctly
+*/
+import { ref } from 'vue'
+function useToggle(flag) {
+  let state = ref(flag)
+
+  function toggle() {
+    state.value = !state.value
+  }
+
+  return [state, toggle]
+}
+
+const [state, toggle] = useToggle(false)
+
+</script>
+
+<template>
+  <p>State: {{ state ? 'ON' : 'OFF' }}</p>
+  <p @click="toggle">
+    Toggle state
+  </p>
+</template>
