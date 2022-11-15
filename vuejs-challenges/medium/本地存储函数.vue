143f991fdf2eaa6b4aa482c6dd26455dac1ce4b2
xuetingfeng <coderxue@qq.com>
2022-11-15 17:13:04 +0800

    本地存储函数

---
+++ vuejs-challenges/medium/本地存储函数.vue
@@ -0,0 +1,36 @@
+<script setup lang='ts'>
+
+import { ref, watchEffect } from "vue"
+
+/**
+ * Implement the composable function
+ * Make sure the function works correctly
+*/
+function useLocalStorage(key: string, initialValue: any) {
+
+  const value = ref(localStorage.getItem(key) || initialValue)
+
+  watchEffect(() => {
+    localStorage.setItem(key, value.value)
+  })
+
+  return value
+}
+
+const counter = useLocalStorage("counter", 0)
+
+// We can get localStorage by triggering the getter:
+console.log(counter.value)
+
+// And we can also set localStorage by triggering the setter:
+
+const update = () => counter.value++
+
+</script>
+
+<template>
+  <p>Counter: {{ counter }}</p>
+  <button @click="update">
+    Update
+  </button>
+</template>
